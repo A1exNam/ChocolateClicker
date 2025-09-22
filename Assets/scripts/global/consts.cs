abc415a9b7a120c588463123cbaadb1ceccfc99e
A1exNam <alexnam16@gmail.com>
2025-09-22 17:26:41 +0300

    debugging + change colors

--- Assets/scripts/global/consts.cs
+++ Assets/scripts/global/consts.cs
@@ -72,7 +72,7 @@ public static class consts{
         tap_indicator_bonus_shake_strength = 6f,
         tap_indicator_bonus_shake_angle = 5f,
         tap_indicator_bonus_shake_speed = 18f,
-        indicator_bonus_tap_outline_width = -1f,
+        indicator_bonus_tap_outline_width = 0.2f,
 
         //active skill
         as_cd = 30f,
--- Assets/scripts/inits/consts_init.cs
+++ Assets/scripts/inits/consts_init.cs
@@ -75,7 +75,5 @@ public class consts_init: MonoBehaviour{
         indicator_bonus_tap_outline_clr,
         active_upgr_clr,
         not_active_upgr_clr;
-
-    public float indicator_bonus_tap_outline_width = -1f;
 }
 
