f143c6606020945dcf6a4d1b232f650abcef3402
A1exNam <103841532+A1exNam@users.noreply.github.com>
2025-09-24 18:27:23 +0300

    Add level-up reward text and payout

--- Assets/scripts/inits/urefs_init.cs
+++ Assets/scripts/inits/urefs_init.cs
@@ -145,6 +145,7 @@ public class urefs_init : MonoBehaviour{
         quest_progress_desc_txt,
         quest_progress_val_txt,
         quest_reward_txt,
+        levelup_banner_reward_txt,
         tutor_click_txt,
         tutor_tap_txt,
         tutor_upgr_txt,
